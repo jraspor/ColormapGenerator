7a6e338135366166bd7b9cf59638070160bf97ce
jraspor <jraspor111@gmail.com>
2020-08-31 22:06:00 +0200

    Interpolation separate files

---
+++ Interpolation.h
@@ -0,0 +1,14 @@
+#ifndef INTERPOLATION_H
+#define INTERPOLATION_H
+
+#include <iostream>
+#include <vector>
+
+class Interpolation {
+public:
+    Interpolation();
+    ~Interpolation();
+    double interpolate();
+};
+
+#endif
